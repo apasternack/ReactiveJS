92e40ffa11aee8784b4afd17bf52e87488a4b445
Adam Pasternack <adam.pasternack@gmail.com>
2017-04-05 20:47:33 -0400

    get items function mock async service request

--- src-client/example_01_basic_ac.js
+++ src-client/example_01_basic_ac.js
@@ -1,1 +1,16 @@
-alert("hello");
+import $ from "jquery";
+
+const $title = $("#title");
+const $results = $("#results");
+
+// --------------------
+//Library
+function getItems(title) {
+    console.log(`Querying ${title}`);
+    return new Promise((resolve, reject) => {
+        window.setTimeout(() => {
+            resolve([title, "Item 2", `Another ${Math.random()}`]);
+        }, 500 + (Math.random() * 200));
+
+    });
+}
