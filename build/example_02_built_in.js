eee72b6c93e925d2d6f7ea56d3803e5e91588992
Adam Pasternack <adam.pasternack@gmail.com>
2017-04-09 20:54:46 -0400

    using .from to perform transforms on an array

--- build/example_02_built_in.js
+++ build/example_02_built_in.js
@@ -12,4 +12,14 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 //     .take(5)
 //     .subscribe(createSubscriber("interval"));
 
-_Rx2.default.Observable.timer(1000, 500).take(3).subscribe((0, _util.createSubscriber)("timer"));
+// Rx.Observable.timer(1000, 500)
+//     .take(3)
+//     .subscribe(createSubscriber("timer"));
+
+_Rx2.default.Observable.of(["this is an array!", "hey"]).subscribe((0, _util.createSubscriber)("of"));
+
+// .from takes an iterable/array like thing and .next returns each of them
+var arr = [1, 2, 3, 4, 5];
+_Rx2.default.Observable.from(arr).map(function (i) {
+    return i * 5;
+}).subscribe((0, _util.createSubscriber)("from"));
--- src-server/example_02_built_in.js
+++ src-server/example_02_built_in.js
@@ -5,7 +5,15 @@ import {createSubscriber} from "./lib/util";
 //     .take(5)
 //     .subscribe(createSubscriber("interval"));
 
-Rx.Observable.timer(1000, 500)
-    .take(3)
-    .subscribe(createSubscriber("timer"));
+// Rx.Observable.timer(1000, 500)
+//     .take(3)
+//     .subscribe(createSubscriber("timer"));
 
+Rx.Observable.of(["this is an array!", "hey"])
+    .subscribe(createSubscriber("of"));
+
+// .from takes an iterable/array like thing and .next returns each of them
+const arr = [1,2,3,4,5];
+Rx.Observable.from(arr)
+    .map(i => i * 5)
+    .subscribe(createSubscriber("from"));
